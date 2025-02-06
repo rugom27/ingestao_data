d0cfd4d70b537467f2645f4aefa9c6b9f2ac224a
Rui Gomes <rui-miguel.gomes@capgemini.com>
2025-02-06 11:25:19 +0000

    Connections problems fixed

--- app/full_app.py
+++ app/full_app.py
@@ -1,26 +1,4 @@
 import streamlit as st
-from db import open_connection, close_connection
-
-
-# ----------------------CONEXÃO À DB------------------
-# Criar um estado no Streamlit para seguir a conexão
-if "db_connection" not in st.session_state:
-    st.session_state["db_connection"] = None
-
-if "force_refresh" not in st.session_state:
-    st.session_state["force_refresh"] = False
-# ----------------------------------------------------
-
-# Criar barra lateral para navegação e controle da conexão
-st.sidebar.markdown("## 🔧 Controlo de Ligação (Base de dados)")
-st.sidebar.markdown("---")
-
-if st.sidebar.button("🟢 Abrir Conexão", key="abrir"):
-    open_connection()
-
-if st.sidebar.button("🔴 Fechar Conexão", key="fechar"):
-    close_connection()
-
 
 pages = {
     "Home": [
